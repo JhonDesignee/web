72f7af4e6b4a05bd318876980689fe70b9592f67
jhonata.oliveira.275@gmail.com <jhonata.oliveira.275@gmail.com>
2022-07-12 19:06:40 -0300

    Atualização

--- maintenance.js
+++ maintenance.js
@@ -1,6 +1,9 @@
 const redirect = true
 
-if (redirect) {
-  sessionStorage.setItem("current_url", window.location.href)
-  window.location = "maintenance.html"
-}
+  if (redirect) {
+    let link = document.querySelector("script[src*=maintenance]").getAttribute("src")
+    link = link.replace(".js", ".html")
+    sessionStorage.setItem("current_url", window.location.href)
+    
+    window.location.href = link
+  }
